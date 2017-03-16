9e903f80a5db09e712813e8f5234af2294525b13
Leon Jessen <leon.jessen@dbac.dk>
2017-03-16 08:22:52 +0100

    elaborations of code and update of readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pi_approximation
+
+An R-script for approximating pi using Monte Carlo simulations.
+
+Given a square with side length 'D', in which a circle is drawn, with diameter 'D', pi can be approximated by the fraction area_circle / area_square = pi x (D/2)^2 / D^2 = pi x D^2 / 4D^2 = pi / 4. Hence if we calculate the above fraction and multiply it with 4, we get pi! Here is an example of throwing 10,000 darts and then doing the before mentioned calculation.
+
+![alt text](pi_approximation.png "Approximation of pi using Monte Carlo simulations")
+
--- pi_approximation.R
+++ pi_approximation.R
@@ -1,3 +1,13 @@
+# ------------------------------------------------------------------------------
+# Given a square with side length 'D', in which a circle is drawn, with
+# diameter 'D', pi can be approximated by the fraction:
+#     area_circle / area_square = pi x (D/2)^2 / D^2
+#                               = pi x D^2 / 4D^2
+#                               = pi / 4
+# Hence if we calculate the above fraction and multiply it with 4, we get pi!
+# Let's do it:
+# ------------------------------------------------------------------------------
+
 # Clear workspace
 # ------------------------------------------------------------------------------
 rm(list=ls())
@@ -8,33 +18,32 @@ library('tidyverse')
 
 # Define functions
 # ------------------------------------------------------------------------------
-throw_dart = function(n=1){
-  return(tibble(x   = runif(n,min = -1, max = 1),
-                y   = runif(n,min = -1, max = 1),
-                hit = factor(ifelse(sqrt(x**2+y**2)<=1,1,0))))
+throw_dart = function(n = 1){
+  return(tibble(x   = runif(n, min = -1, max = 1),
+                y   = runif(n, min = -1, max = 1),
+                hit = factor( ifelse(sqrt(x**2 + y**2) <= 1, 1, 0) ))
+         )
 }
 
 # Set random seed
 # ------------------------------------------------------------------------------
 set.seed(201461)
 
-# Compute pi using monte carlo approximation
+# Compute pi using monte carlo simulations
 # ------------------------------------------------------------------------------
-darts   = throw_dart(10000)
+n_sims  = 10000
+darts   = throw_dart(n_sims)
 pi_appr = darts %>%
-  select(hit) %>%
-  as_vector %>%
-  unname %>%
-  as.character %>%
-  as.numeric %>%
-  mean * 4
+  summarise(pi_appr = mean(as.numeric(hit)-1)) %>%
+  as_vector * 4
 
 # Plot results
 # ------------------------------------------------------------------------------
 label = paste("pi == ", pi_appr)
 darts %>% 
-  ggplot(aes(x=x,y=y,colour=hit)) +
+  ggplot(aes(x = x, y = y, colour = hit)) +
   geom_point() +
-  annotate("text",x=0,y=0,label=label,parse=TRUE) +
+  annotate("text", x = 0, y = 0, label = label, parse = TRUE) +
   coord_fixed() +
   theme_bw()
+ggsave('pi_approximation.png')
